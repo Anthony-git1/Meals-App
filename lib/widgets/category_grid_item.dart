ab7cc0a833c75c03bf9d482d1cdde8fabf5e86e6
Anthony-git1 <medhatanthony3@gmail.com>
2025-01-15 18:33:06 +0200

    categories created

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'package:meals_app/model/category.dart';
+
+class CategoryGridItem extends StatelessWidget{
+  const CategoryGridItem ({super.key, required this.category});
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            category.color.withOpacity(0.5),
+            category.color.withOpacity(0.95)
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Text(category.title, style: Theme.of(context).textTheme.titleLarge!.copyWith(
+          color: Theme.of(context).colorScheme.onBackground,
+        ),
+      ),
+    );
+  }
+}
