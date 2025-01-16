7519cf1dbe30ab9a5a130fc9867358c3b70c1073
Anthony-git1 <medhatanthony3@gmail.com>
2025-01-16 16:02:31 +0200

    added meals screen

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -9,20 +9,26 @@ class CategoryGridItem extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.5),
-            category.color.withOpacity(0.95)
-          ],
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
+    return InkWell(
+      onTap: () {},
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.5),
+              category.color.withOpacity(0.95)
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            ),
           ),
-        ),
-        child: Text(category.title, style: Theme.of(context).textTheme.titleLarge!.copyWith(
-          color: Theme.of(context).colorScheme.onBackground,
+          child: Text(category.title, style: Theme.of(context).textTheme.titleLarge!.copyWith(
+            color: Theme.of(context).colorScheme.onBackground,
+          ),
         ),
       ),
     );
