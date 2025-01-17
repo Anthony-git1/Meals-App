82e661c338e3a0b6a9a933bdc19e6a1c31800b2e
Anthony-git1 <medhatanthony3@gmail.com>
2025-01-17 16:10:02 +0200

    meal item traits

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
-
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/screens/categories.dart';
-import 'package:meals_app/screens/meals.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
   colorScheme: ColorScheme.fromSeed(
     brightness: Brightness.dark,
-    seedColor: const Color.fromARGB(255, 131, 57, 0),
+    seedColor: const Color.fromARGB(255, 0, 0, 0),
   ),
   textTheme: GoogleFonts.latoTextTheme(),
 );
@@ -25,10 +22,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const MealsScreen(
-        title: 'Some category...',
-        meals: dummyMeals,
-      ),
+      home: const CategoriesScreen()
     );
   }
 }
--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -2,15 +2,20 @@ import 'package:flutter/material.dart';
 
 import 'package:meals_app/model/category.dart';
 
-class CategoryGridItem extends StatelessWidget{
-  const CategoryGridItem ({super.key, required this.category});
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({
+    super.key,
+    required this.category,
+    required this.onSelectCategory,
+  });
 
   final Category category;
+  final void Function() onSelectCategory;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: onSelectCategory,
       splashColor: Theme.of(context).primaryColor,
       borderRadius: BorderRadius.circular(16),
       child: Container(
@@ -22,13 +27,15 @@ class CategoryGridItem extends StatelessWidget{
               category.color.withOpacity(0.5),
               category.color.withOpacity(0.95)
             ],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            ),
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
           ),
-          child: Text(category.title, style: Theme.of(context).textTheme.titleLarge!.copyWith(
-            color: Theme.of(context).colorScheme.onBackground,
-          ),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
         ),
       ),
     );
